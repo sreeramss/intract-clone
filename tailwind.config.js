7d0ab4af1b97c24e9e590113256d8037a7f73b75
Sree Ram  S S <89720320+sreeramss@users.noreply.github.com>
2024-07-09 23:14:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGray: 'rgb(24, 24, 24)',
+      },
+    },
+  },
+  fontFamily: {
+    outfit: ["Outfit", "sans-serif"],
+  },
+  plugins: [],
+}
